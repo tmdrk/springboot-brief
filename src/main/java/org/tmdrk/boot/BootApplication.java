5d65bc81f6b5a06c905b0e3c0cd8436afcb7a6e0
zhoujie <1308398245@qq.com>
2020-07-21 17:25:35 +0800

    首次提交

---
+++ src/main/java/org/tmdrk/boot/BootApplication.java
@@ -0,0 +1,22 @@
+package org.tmdrk.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boot")
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+
+    @RequestMapping("/index")
+    String index() {
+        return "Hello Spring Boot";
+    }
+}
