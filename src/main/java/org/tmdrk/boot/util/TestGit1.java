642e704aa9f63c86ca90e5e95c154bbfeaf9c1b0
zhoujie <1308398245@qq.com>
2020-07-21 18:11:28 +0800

    git测试提交

---
+++ src/main/java/org/tmdrk/boot/util/TestGit1.java
@@ -0,0 +1,18 @@
+package org.tmdrk.boot.util;
+
+/**
+ * @ClassName TestGit1
+ * @Description TODO
+ * @Author zhoujie
+ * @Date 2020/7/21 18:06
+ * @Version 1.0
+ **/
+public class TestGit1 {
+    public static void main(String[] args) {
+        System.out.println("你好这是一个测试");
+        for(int i=0;i<10;i++){
+            System.out.println("循环次数："+i);
+        }
+        System.out.println("end 结束");
+    }
+}
---
+++ src/main/java/org/tmdrk/boot/util/TestGit2.java
@@ -0,0 +1,20 @@
+package org.tmdrk.boot.util;
+
+/**
+ * @ClassName TestGit2
+ * @Description TODO
+ * @Author zhoujie
+ * @Date 2020/7/21 18:08
+ * @Version 1.0
+ **/
+public class TestGit2 {
+    int i = 9;
+
+    public static void main(String[] args) {
+        System.out.println("你好这是一个测试");
+        for(int i=0;i<10;i++){
+            System.out.println("循环次数："+i);
+        }
+        System.out.println("end 结束");
+    }
+}
