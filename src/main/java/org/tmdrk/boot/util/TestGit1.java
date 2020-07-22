8123ec1007a1607123075f1c580c526ea23ced98
zhoujie <1308398245@qq.com>
2020-07-23 00:15:23 +0800

    git测试111

--- src/main/java/org/tmdrk/boot/util/TestGit1.java
+++ src/main/java/org/tmdrk/boot/util/TestGit1.java
@@ -9,7 +9,7 @@ package org.tmdrk.boot.util;
  **/
 public class TestGit1 {
     public static void main(String[] args) {
-        System.out.println("你好这是一个测试hh");
+        System.out.println("你好这是一个测试hh111");
         for(int i=0;i<10;i++){
             System.out.println("循环次数："+i+" 次");
         }
