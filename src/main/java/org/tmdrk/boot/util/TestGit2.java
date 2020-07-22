d61bc6b3bc0b5a40d533dd90a69e13d9311c4baf
zhoujie <1308398245@qq.com>
2020-07-23 00:50:20 +0800

    testGit222

--- src/main/java/org/tmdrk/boot/util/TestGit2.java
+++ src/main/java/org/tmdrk/boot/util/TestGit2.java
@@ -11,7 +11,7 @@ public class TestGit2 {
     int i = 92;
 
     public static void main(String[] args) {
-        System.out.println("你好这是一个测试11");
+        System.out.println("你好这是一个测试11222");
         for(int i=0;i<10;i++){
             System.out.println("循环次数 第："+i+"次");
         }
